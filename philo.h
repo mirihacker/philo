70f6e0bc98c260d4105b352581a074d160de29d5
Siria <siria_miren@icloud.com>
2024-10-08 16:55:45 +0200

    final no final

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: smiranda <smiranda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 13:52:42 by smiranda          #+#    #+#             */
-/*   Updated: 2024/10/08 16:50:34 by smiranda         ###   ########.fr       */
+/*   Updated: 2024/10/08 16:54:36 by smiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,8 @@
 # define STD "\033[0m"
 # define RED "\033[1;31m"
 # define G "\033[1;32m"
-# define Y "\033[1;33m"
-# define B "\033[1;34m"
-# define M "\033[1;35m"
 # define C "\033[1;36m"
 # define W "\033[1;37m"
-
-# define DEBUGG_MODE 0
 
 typedef enum e_status
 {
